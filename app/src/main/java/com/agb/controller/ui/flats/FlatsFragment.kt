0102914aa6c00bd63c229401a9e9cb12803fb704
NeProgramist <zaskozhenya@gmail.com>
2020-10-15 09:24:54 +0300

    feat(app): add primary flats ui

--- app/src/main/java/com/agb/controller/ui/flats/FlatsFragment.kt
+++ app/src/main/java/com/agb/controller/ui/flats/FlatsFragment.kt
@@ -1,17 +1,51 @@
 package com.agb.controller.ui.flats
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.agb.controller.R
+import com.agb.controller.framework.datasource.remote.api.FlatsApi
+import com.agb.core.common.Result
+import com.agb.core.common.Status
+import com.agb.core.domain.model.Flat
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.schedulers.Schedulers
+import kotlinx.android.synthetic.main.flats_fragment.*
 
 class FlatsFragment : Fragment() {
-
     private lateinit var viewModel: FlatsViewModel
+    private lateinit var flatsAdapter: FlatsAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.flats_fragment, container, false)
+    }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(FlatsViewModel::class.java)
 
+        flatsAdapter = FlatsAdapter()
+        viewModel = ViewModelProvider(this).get(FlatsViewModel::class.java)
+        viewModel.flats.observe(viewLifecycleOwner, flatsObserver)
+        viewModel.flat.observe(viewLifecycleOwner, flatObserver)
+
+        flat_rv.adapter = flatsAdapter
+        flat_rv.layoutManager = LinearLayoutManager(activity)
+        viewModel.getFlats()
     }
 
+    private val flatsObserver = Observer<Result<List<Flat>>> { flats ->
+        flats.data?.let { if (flats.status == Status.SUCCESS) flatsAdapter.addFlats(it) }
+    }
+
+    private val flatObserver = Observer<Result<Flat>> { flat ->
+        flat.data?.let { if (flat.status == Status.SUCCESS) flatsAdapter.addFlat(it) }
+    }
 }
--- app/src/main/java/com/agb/controller/ui/flats/FlatsViewModel.kt
+++ app/src/main/java/com/agb/controller/ui/flats/FlatsViewModel.kt
@@ -1,7 +1,48 @@
 package com.agb.controller.ui.flats
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.agb.controller.framework.datasource.remote.FlatsRemoteDataSource
+import com.agb.controller.framework.datasource.remote.api.FlatsApi
+import com.agb.core.domain.interactor.AddFlatUseCase
+import com.agb.core.domain.interactor.GetFlatsUseCase
+import com.agb.core.domain.model.Flat
+import com.agb.core.common.Result
+import com.agb.core.domain.repository.flats.FlatsRepositoryImpl
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 
 class FlatsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    val flats = MutableLiveData<Result<List<Flat>>>()
+    val flat = MutableLiveData<Result<Flat>>()
+
+    fun getFlats() {
+        GetFlatsUseCase(
+            FlatsRepositoryImpl(
+                FlatsRemoteDataSource(
+                    FlatsApi()
+                )
+            )
+        )()
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { flats.value = Result.success(it) },
+                { flats.value = Result.error(it) }
+            )
+    }
+
+    fun addFlat() {
+        val mock = Flat(0, "405", "Some where in the space")
+        AddFlatUseCase(
+            FlatsRepositoryImpl(
+                FlatsRemoteDataSource(
+                    FlatsApi()
+                )
+            )
+        )(mock)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { flat.value = Result.success(it) },
+                { flats.value = Result.error(it) }
+            )
+    }
 }
