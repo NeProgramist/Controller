1bd77dcf25e2af0230a1202f9d95ada40bcce18b
NeProgramist <zaskozhenya@gmail.com>
2020-10-15 09:23:48 +0300

    feat(app): add primary login ui

--- app/src/main/java/com/agb/controller/ui/MainActivity.kt
+++ app/src/main/java/com/agb/controller/ui/MainActivity.kt
@@ -1,13 +1,42 @@
 package com.agb.controller.ui
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.agb.controller.R
+import com.agb.controller.framework.App
+import com.agb.controller.framework.datasource.local.PreferencesManager
+import com.agb.controller.ui.flats.FlatsFragment
+import com.agb.controller.ui.flats.FlatsViewModel
+import com.agb.controller.ui.login.LoginFragment
+import com.agb.controller.ui.login.LoginViewModel
+import com.agb.core.common.Result
+import com.agb.core.common.Status
+import com.agb.core.domain.model.User
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel.user.observe(this, userObserver)
+        viewModel.getUserData()
+    }
+
+    fun changeFragment(fragment: Fragment) {
+        val ft = supportFragmentManager.beginTransaction()
+        ft.replace(R.id.fragment_container, fragment)
+        ft.commit()
+    }
+
+    private val userObserver = Observer<Result<User>> { result ->
+        if (result.status == Status.SUCCESS) changeFragment(FlatsFragment())
+        else changeFragment(LoginFragment())
     }
 }
