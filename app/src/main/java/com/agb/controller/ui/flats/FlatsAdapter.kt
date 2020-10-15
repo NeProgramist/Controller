46e87e1eb2d95c013f703186b32f421d4cb29e36
NeProgramist <zaskozhenya@gmail.com>
2020-10-15 09:47:26 +0300

    feat(app): add recycler view

---
+++ app/src/main/java/com/agb/controller/ui/flats/FlatsAdapter.kt
@@ -0,0 +1,60 @@
+package com.agb.controller.ui.flats
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.agb.controller.R
+import com.agb.core.domain.model.Flat
+import kotlinx.android.synthetic.main.flat.view.*
+
+class FlatsAdapter(
+//    private val onItemClick: OnItemClickListener
+): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private var flatsHolder = ArrayList<Flat>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val item = layoutInflater.inflate(R.layout.flat, parent, false)
+        return FlatViewHolder(item)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is FlatViewHolder) {
+            holder.bind(flatsHolder[position])
+        }
+    }
+
+    override fun getItemCount() = flatsHolder.size
+
+    fun addFlat(flat: Flat) {
+        flatsHolder.add(flat)
+        notifyItemInserted(flatsHolder.size - 1)
+    }
+
+    fun addFlats(flats: List<Flat>) {
+        val startIndex = flats.size - 1
+        flatsHolder.addAll(flats)
+        notifyItemRangeInserted(startIndex, flats.size)
+    }
+
+    fun clear() {
+        val size = flatsHolder.size
+        flatsHolder.clear()
+        notifyItemRangeRemoved(0, size - 1)
+    }
+
+    inner class FlatViewHolder(
+        private val item: View,
+//        private val onItemClick: OnItemClickListener
+    ): RecyclerView.ViewHolder(item) {
+        fun bind(flat: Flat) {
+            item.flat_name_txt.text = flat.name
+            item.flat_address_txt.text = flat.address
+        }
+    }
+
+    interface OnItemClickListener {
+        fun onClickListener(id: Int)
+    }
+}
